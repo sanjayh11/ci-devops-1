f645f153c1734fa80e64d28dce6e66a8150fba16
sanjayh11 <35250547+sanjayh11@users.noreply.github.com>
2018-01-23 10:16:58 +0530

    First Set of commit to remote GitHub server - Sanjay
    Signed-off-by: sanjayh11 <35250547+sanjayh11@users.noreply.github.com>

---
+++ sanjay-devops/src/main/java/com/devops1/business/FirstSampleJavaFile.java
@@ -0,0 +1,14 @@
+package com.devops1.business;
+
+public class FirstSampleJavaFile {
+
+	public FirstSampleJavaFile() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ sanjay-devops/src/test/java/TestFirstSampleJavaFile.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestFirstSampleJavaFile {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAge() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testName() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testGender() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testIsDatabaseConnected() {
+		fail("Not yet implemented");
+	}
+}
